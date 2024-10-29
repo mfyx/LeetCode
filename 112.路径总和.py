a53b390bf46afcfae6a06a16d1f2ad1e03ae54f1
mfyx <1571547820@qq.com>
2024-10-29 21:18:38 +0800

    solve 112.路径总和

---
+++ 112.路径总和.py
@@ -0,0 +1,27 @@
+#
+# @lc app=leetcode.cn id=112 lang=python3
+#
+# [112] 路径总和
+#
+
+# @lc code=start
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def hasPathSum(self, root: Optional[TreeNode], targetSum: int) -> bool:
+        if not root:
+            return False
+        
+        if not root.left and not root.right:
+            return root.val == targetSum
+        
+        targetSum -= root.val
+        return self.hasPathSum(root.left, targetSum) or self.hasPathSum(root.right, targetSum)
+
+        
+# @lc code=end
+
